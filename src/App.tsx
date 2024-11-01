30ba19ff32c036e220a7d905ec43f9ce83d3743f
venkatasrikarakella <srikarvishnudatta@gmail.com>
2024-11-01 13:29:59 -0400

    added some animations and fixed bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import About from "./components/About"
 import Experience from "./components/Experience"
 import ProjectsComponent from "./components/ProjectsComponent"
-
+import 'animate.css'
 
 function App() {
 
   return (
-    <div className="main-container">
+    <div className="animate__animated animate__fadeInUp main-container">
       <About />
       <Experience />
       <ProjectsComponent />
